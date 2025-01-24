49414a31141ecf7abd1054567393c65e356593f0
sanketswani <sankeyswani@gmail.com>
2025-01-24 19:53:30 +0100

    removing existing rg

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform { 
+  cloud { 
+    
+    organization = "ApnaCompany" 
+
+    workspaces { 
+      name = "ws-api-based-sanket-002" 
+    } 
+  } 
+}
+
 provider "azurerm" {
   features {}
 
@@ -7,10 +18,10 @@ provider "azurerm" {
   client_secret   = var.client_secret
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg-new-weu-nonprod-001"
-  location = "West Europe"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "rg-new-weu-nonprod-001"
+#   location = "West Europe"
+# }
 
 variable "subscription_id" {}
 variable "client_id" {}
