79eb51d5bb86a84ce30182a5b8fbd73d2a3b79aa
sanketswani <sankeyswani@gmail.com>
2025-01-24 17:56:11 +0100

    updating variables for azure subscriptions

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features {}
 
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # tenant_id       = var.tenant_id
-  # client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
 }
 
 resource "azurerm_resource_group" "example" {
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-# variable "subscription_id" {}
-# variable "client_id" {}
-# variable "tenant_id" {}
-# variable "client_secret" {}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "tenant_id" {}
+variable "client_secret" {}
