b9ddd423c3dc6d4db6cb01e61561c9552e3a1fbc
sanketswani <sankeyswani@gmail.com>
2025-01-24 17:54:23 +0100

    updating resource group name

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  tenant_id       = var.tenant_id
-  client_secret   = var.client_secret
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # tenant_id       = var.tenant_id
+  # client_secret   = var.client_secret
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+  name     = "rg-new-weu-nonprod-001"
+  location = "West Europe"
 }
 
-variable "subscription_id" {}
-variable "client_id" {}
-variable "tenant_id" {}
-variable "client_secret" {}
+# variable "subscription_id" {}
+# variable "client_id" {}
+# variable "tenant_id" {}
+# variable "client_secret" {}
