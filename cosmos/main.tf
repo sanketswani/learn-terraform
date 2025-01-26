27f33228f4432308945d658f9c71d0383fe3e445
sanketswani <sankeyswani@gmail.com>
2025-01-26 20:08:00 +0100

    changing folder structure

--- cosmos/main.tf
+++ cosmos/main.tf
@@ -23,7 +23,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "rg-new-weu-nonprod-002"
   location = "West Europe"
-  tags     = { "created-by" = "terraform" }
+  tags     = { "created-by" = "terraform", "createdFor" = "Cosmos" }
 }
 
 variable "subscription_id" {}
