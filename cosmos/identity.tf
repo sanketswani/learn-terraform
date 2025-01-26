4a1e2bbadc4e2f2f6092f3cc9d874fda8d76ce57
sanketswani <sankeyswani@gmail.com>
2025-01-26 21:27:00 +0100

    updating folder structure

--- cosmos/identity.tf
+++ cosmos/identity.tf
@@ -1,4 +1,4 @@
-resource "azurerm_user_assigned_identity" "automation_account" {
+resource "azurerm_user_assigned_identity" "automation_account_identity" {
   resource_group_name = azurerm_resource_group.example.name
   name                = "identity1"
   location            = azurerm_resource_group.example.location
