9eb37b143f51e7878463a5bb9c9176b16aaa0303
sanketswani <sankeyswani@gmail.com>
2025-01-28 20:06:25 +0100

    updating folder name along with new resource

--- cosmos/automation_account/automation_account.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "example" {
-  name     = "rg-weu-nonprod-003"
-  location = "West Europe"
-  tags     = { "created-by" = "terraform", "createdFor" = "automation account" }
-}
---
+++ cosmos_automation_setup/automation_account.tf
@@ -0,0 +1,11 @@
+resource "azurerm_automation_account" "automation_account" {
+  name                = "automation-account-001"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku_name            = "Basic"
+  identity {
+    type         = "UserAssigned"
+    identity_ids = azurerm_user_assigned_identity.automation_account_identity.id
+  }
+  tags = { "CreatedBy" = "terraform" }
+}
