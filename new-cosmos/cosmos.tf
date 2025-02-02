8ecafbf9b76bffb7547e0fa5919695f9e82e656d
sanketswani <sankeyswani@gmail.com>
2025-02-02 19:08:41 +0100

    changing formatting

--- new-cosmos/cosmos.tf
+++ new-cosmos/cosmos.tf
@@ -1,7 +1,7 @@
 module "cosmos-db-account" {
-  source       = "app.terraform.io/ApnaCompany/cosmos-db-account/azure"
-  version      = "0.1.0"
-  account_name = "cos"
+  source         = "app.terraform.io/ApnaCompany/cosmos-db-account/azure"
+  version        = "0.1.0"
+  account_name   = "cos"
   resource_group = "rg-new-weu-nonprod-002"
-  location = "West Europe"
+  location       = "West Europe"
 }
