dbc5f3ec1b9550b0d6385403470dbc2b2c737470
sanketswani <sankeyswani@gmail.com>
2025-02-02 19:07:31 +0100

    adding required variables

--- new-cosmos/cosmos.tf
+++ new-cosmos/cosmos.tf
@@ -2,4 +2,6 @@ module "cosmos-db-account" {
   source       = "app.terraform.io/ApnaCompany/cosmos-db-account/azure"
   version      = "0.1.0"
   account_name = "cos"
+  resource_group = "rg-new-weu-nonprod-002"
+  location = "West Europe"
 }
