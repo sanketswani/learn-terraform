79f554fc9e589bd7646efe2ebcba5aea43d6f28f
sanketswani <sankeyswani@gmail.com>
2025-02-02 19:04:26 +0100

    updating formatting

--- new-cosmos/cosmos.tf
+++ new-cosmos/cosmos.tf
@@ -1,5 +1,5 @@
 module "cosmos-db-account" {
-  source  = "app.terraform.io/ApnaCompany/cosmos-db-account/azure"
-  version = "0.1.0"
+  source       = "app.terraform.io/ApnaCompany/cosmos-db-account/azure"
+  version      = "0.1.0"
   account_name = "cos"
 }
