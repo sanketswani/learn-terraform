a76ba61439639dbf383887aff9cb838a003dbbc7
sanketswani <sankeyswani@gmail.com>
2025-02-02 19:15:15 +0100

    adding new file in exisiting setup

--- cosmos_automation_setup/new-cosmos.tf
+++ cosmos_automation_setup/new-cosmos.tf
@@ -2,6 +2,6 @@ module "cosmos-db-account" {
   source         = "app.terraform.io/ApnaCompany/cosmos-db-account/azure"
   version        = "0.1.0"
   account_name   = "cosmos-account-for-mongo-002"
-  resource_group = "rg-new-weu-nonprod-002"
-  location       = "West Europe"
+  resource_group = azurerm_resource_group.example.name
+  location       = azurerm_resource_group.example.location
 }
