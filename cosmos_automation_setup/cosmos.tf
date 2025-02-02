11591216d14133b93cefcd1bf40ecc9637db1125
sanketswani <sankeyswani@gmail.com>
2025-02-02 19:56:30 +0100

    using latest module of cosmos

--- cosmos_automation_setup/cosmos.tf
+++ cosmos_automation_setup/cosmos.tf
@@ -25,7 +25,7 @@
 
 module "cosmos-db-account" {
   source           = "app.terraform.io/ApnaCompany/cosmos-db-account/azure"
-  version          = "0.1.0"
+  version          = "0.2.0"
   account_name     = "cosmos-mongo-account-002"
   resource_group   = azurerm_resource_group.example.name
   location         = azurerm_resource_group.example.location
