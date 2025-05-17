190bc96745ffa8ebe6e330be2118a305717bfdb6
sanketswani <sankeyswani@gmail.com>
2025-05-17 11:01:16 +0200

    importing existing resource

--- static-web-app/static-web-app.tf
+++ static-web-app/static-web-app.tf
@@ -8,3 +8,14 @@ resource "azurerm_static_web_app" "portfolio-static-web-app" {
   sku_tier                           = "Free"
   sku_size                           = "Free"
 }
+
+resource "azurerm_static_web_app_old" "portfolio-static-web-app_old" {
+  name                               = "my-portfolio"
+  resource_group_name                = "rg-portfolio-content"
+  location                           = "West Europe"
+  configuration_file_changes_enabled = true
+  preview_environments_enabled       = true
+  public_network_access_enabled      = true
+  sku_tier                           = "Free"
+  sku_size                           = "Free"
+}
